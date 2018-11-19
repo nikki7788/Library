e2686950b9681eea96827fec2bdc7b5aa994bb85
NIKBAKHT\sarir <hn0297@gmail.com>
2018-11-19 20:12:26 +0330

    to define   the view of the user controller-index action as to show users registered

--- Library Project/Library Project/Areas/Admin/Controllers/UserController.cs
+++ Library Project/Library Project/Areas/Admin/Controllers/UserController.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Library.Models;
+using Library.Models.ViewModel;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.Area.Admin.Controllers
@@ -9,9 +12,33 @@ namespace Library.Area.Admin.Controllers
     [Area("Admin")]
     public class UserController : Controller
     {
-        public IActionResult Test1()
+        //Dependency injection for identity role and identity user
+
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        public UserController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
         {
-            return View();
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //creating a list of view model
+            List<UserListViewModel> model = new List<UserListViewModel>();
+
+            //at first we go to the  database(_userManager)-Users' Table and then selcting properties of Users' table and
+            //converting them to the view model type(u => new UserListViewModel) after that put them in properties of vie model(e.g.: Id = u.Id)
+            //u => refer to Users table
+            model = _userManager.Users.Select(u => new UserListViewModel
+            {
+                Id = u.Id,
+                Name = u.FirstName,
+                Email=u.Email
+            }).ToList();
+
+            return View(model);
         }
     }
 }
---
+++ Library Project/Library Project/Models/ViewModel/UserListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.ViewModel
+{
+    public class UserListViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string RoleName { get; set; }
+
+
+    }
+}
