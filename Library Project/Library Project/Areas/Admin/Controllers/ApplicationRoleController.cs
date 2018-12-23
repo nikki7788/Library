e8d05d1bf2cc40bb66f9a23a7e88ed2f91bf6f09
NIKBAKHT\sarir <hn0297@gmail.com>
2018-12-23 21:43:58 +0330

    completing the post method  and it's modal of the applicationrole

--- Library Project/Library Project/Areas/Admin/Controllers/ApplicationRoleController.cs
+++ Library Project/Library Project/Areas/Admin/Controllers/ApplicationRoleController.cs
@@ -43,24 +43,63 @@ namespace Library.Areas.Admin.Controllers
         }
         //وقتی روی افزودن نقش جدید و ویرایش کلیک میکنیم مودال مربوط به رول را باز میکند
         [HttpGet]
-        public async Task <IActionResult> AddEditRole(string id)
+        public async Task<IActionResult> AddEditRole(string id)
         {
             ApplicationRoleViewModel model = new ApplicationRoleViewModel();
 
-            ApplicationRole applicationRole =await _roleManager.FindByIdAsync(id);
 
-            if (!string.IsNullOrEmpty(id))
+
+            if (!String.IsNullOrEmpty(id))
             {
-                model.Id = applicationRole.Id;
-                model.Name = applicationRole.Name;
-                model.Description = applicationRole.Description;
+                ApplicationRole applicationRole = await _roleManager.FindByIdAsync(id);
+                if (applicationRole != null)  //**
+                {
+                    model.Id = applicationRole.Id;
+                    model.Name = applicationRole.Name;
+                    model.Description = applicationRole.Description;
+                }
+                else
+                {
+                    return RedirectToAction("Index");
+                }
             }
-            if (applicationRole==null)
-            {
-                return RedirectToAction("Index");
-            }
-            return PartialView("_AddEditApplicationRolePartial",model);
+
+            //نمیخواهد چون  else
+            //(**) if برای  
+            //ریترن ننوشتیم و اگر ان برقرار باشد می اید سراغ این خط کد
+            return PartialView("_AddEditApplicationRolePartial", model);
+
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddEditRole(ApplicationRoleViewModel model, string id, string redirectUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                //String is a class
+                //string is a data type
+                //String.IsNullOrEmpty(id) :if id is null or empty ,it will return true then because of " ! " will return true
+
+                bool isExist = !String.IsNullOrEmpty(id);
+                ApplicationRole applicationRole = isExist ? await _roleManager.FindByIdAsync(id) //updating
+                    : new ApplicationRole(); //inserting
+
+                applicationRole.Name = model.Name;
+                applicationRole.Description = model.Description;
+
+                //it will return result of the updating or inserting that it will be succeeded or not succeeded
+                IdentityResult roleResult = isExist ? await _roleManager.UpdateAsync(applicationRole)  //updating in the database
+                    : await _roleManager.CreateAsync(applicationRole);    //inserting in the database
+
+                if (roleResult.Succeeded)
+                {
+                    //showing Index action and view
+                    return PartialView("_SuccessfullyResponsePartial", redirectUrl);
+                }
+            }
+            //showing addedit modal and it's values and errors
+            return PartialView("_AddEditApplicationRolePartial", model);
+        }
     }
 }
