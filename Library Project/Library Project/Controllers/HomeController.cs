7b262be93f7e4feec884e732907b891057838f98
NIKBAKHT\sarir <hn0297@gmail.com>
2019-01-16 16:25:26 +0330

    competing last book partial and it's model and create a viewmodel for index model

--- Library Project/Library Project/Controllers/HomeController.cs
+++ Library Project/Library Project/Controllers/HomeController.cs
@@ -5,14 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Library.Models;
+using Library.Models.ViewModel;
 
 namespace Library.Controllers
 {
+
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IServiceProvider _serviceProvider;
+        public HomeController(ApplicationDbContext context, IServiceProvider serviceProvider)
+        {
+            _context = context;
+            _serviceProvider = serviceProvider;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            //  شش کتاب اخر وارد شده را برمیکرداند  
+            var model = new MultiModelsViewModel();
+            model.LastBook = (from b in _context.Books orderby b.BookId descending select b).Take(6).ToList();
+
+            //ارسال تصویر به ویو
+            ViewBag.imgPath = "/upload/normalimage/";
+            return View(model);
         }
 
         public IActionResult About()
---
+++ Library Project/Library Project/Models/ViewModel/MultiModelsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.Models.ViewModel
+{
+    public class MultiModelsViewModel
+    {
+        public List<Book> LastBook { get; set; }            //مدل بوک را برمی گرداند
+
+
+    }
+}
